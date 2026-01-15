2681a170171ef9e7bcc7dbe7e536fe037a632577
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-15 15:41:50 +0100

    add default variables

---
+++ product-list/src/app/shared/components/product-detail/product-detail.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail',
+  imports: [],
+  templateUrl: './product-detail.html',
+  styleUrl: './product-detail.scss',
+})
+export class ProductDetail {
+  detail = {
+    "name": "Gaming Maus",
+    "description": "Eine hochwertige Gaming Maus mit programmierbaren Tasten und RGB-Beleuchtung.",
+    "specs": "- DPI: 16000\n- Gewicht: 120g\n- Anschluss: USB-C\n- Tasten: 8 programmierbare Tasten",
+    "stock": 120,
+    "price": 59.99
+  }
+}
--- product-list/src/app/shared/components/product-list/product-list.ts
+++ product-list/src/app/shared/components/product-list/product-list.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.scss',
 })
 export class ProductList {
-
+  list = [
+    {
+      "name": "Gaming Maus",
+      "description": "Eine hochwertige Gaming Maus mit programmierbaren Tasten und RGB-Beleuchtung.",
+      "specs": "- DPI: 16000\n- Gewicht: 120g\n- Anschluss: USB-C\n- Tasten: 8 programmierbare Tasten",
+      "stock": 120,
+      "price": 59.99
+    }
+  ];
 }
